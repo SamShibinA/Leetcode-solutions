7f86e7f94790eb28c2f6a5c7a2b2fc3a36a3c8c7
SAMSHIBN A <151643144+SamShibinA@users.noreply.github.com>
2025-06-24 11:43:41 +0530

    Time: 7 ms (37.95%), Space: 44.8 MB (26.93%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,22 +1,31 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        Set<Integer> ans=new HashSet<>();
-        int l=digits.length;
+        Set<Integer> ans = new HashSet<>();
+        int len = digits.length;
+        recurse(0, 0, 0, digits, len, ans);
+        return ans.size();
+    }
 
-        for(int i=0;i<l;i++){
-            for(int j=0;j<l;j++){
-                for(int k=0;k<l;k++){
-                    if(i!=j&&j!=k&&i!=k){
-                        int n=digits[i]*100+digits[j]*10+digits[k];
-                        if(n%2==0&&n>=100&&n<=999){
-                            ans.add(n);
-                        }
-                    }
-                }
+    private void recurse(int i, int j, int k, int[] digits, int len, Set<Integer> ans) {
+        if (i >= len) return;
+
+        if (j >= len) {
+            recurse(i + 1, 0, 0, digits, len, ans);
+            return;
+        }
+
+        if (k >= len) {
+            recurse(i, j + 1, 0, digits, len, ans);
+            return;
+        }
+
+        if (i != j && j != k && i != k) {
+            int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+            if (num >= 100 && num % 2 == 0) {
+                ans.add(num);
             }
         }
 
-        return ans.size();
+        recurse(i, j, k + 1, digits, len, ans);  // move to next k
     }
 }
-
