c82c0efb522b945bf1189620116c24493078958a
SAMSHIBN A <151643144+SamShibinA@users.noreply.github.com>
2025-06-30 14:43:32 +0530

    Time: 0 ms (100%), Space: 44.3 MB (76.61%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -15,13 +15,14 @@ public class Solution {
 
         ListNode fast=head;
         ListNode slow=head;
-        
-        while(fast!=null && fast.next!=null){
+
+        while(fast!=null&&fast.next!=null){
             fast=fast.next.next;
             slow=slow.next;
             if(fast==slow) return true;
         }
 
         return false;
+
     }
 }
