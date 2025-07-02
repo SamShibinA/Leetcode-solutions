1efbe95d2ed432f1e3f56ccb9305c5ca56ed11b0
SAMSHIBN A <151643144+SamShibinA@users.noreply.github.com>
2025-07-02 11:19:10 +0530

    Time: 3 ms (22.65%), Space: 58.6 MB (31.82%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -1,60 +1,19 @@
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        // Dummy node to simplify edge cases
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
+        ListNode tem=head,first=head,second=head;
+        int n=getLength(tem);
+        
+        for(int i=1;i<k;i++)first=first.next;
 
-        int n = getLength(head);
+        for(int i=1;i<n-k+1;i++)second=second.next;
 
-        // Pointers to previous nodes
-        ListNode prev1 = dummy, prev2 = dummy;
+        int temp=first.val;
+        first.val=second.val;
+        second.val=temp;
 
-        // Find prev1 (before k-th from start)
-        for (int i = 1; i < k; i++) {
-            prev1 = prev1.next;
+        return head;
+
         }
-
-        // Find prev2 (before k-th from end)
-        for (int i = 1; i < n - k + 1; i++) {
-            prev2 = prev2.next;
-        }
-
-        // Nodes to swap
-        ListNode node1 = prev1.next;
-        ListNode node2 = prev2.next;
-
-        // If same node, no need to swap
-        if (node1 == node2) return head;
-
-        // Handle adjacent nodes
-        if (prev1.next == prev2) {
-            // Ensure node1 comes before node2
-            ListNode temp = node1; node1 = node2; node2 = temp;
-            ListNode tempPrev = prev1; prev1 = prev2; prev2 = tempPrev;
-        }
-
-        if (prev1.next == node1 && node1.next == node2) {
-            // Adjacent node1 -> node2
-            node1.next = node2.next;
-            node2.next = node1;
-            prev1.next = node2;
-        } else if (prev2.next == node2 && node2.next == node1) {
-            // Adjacent node2 -> node1
-            node2.next = node1.next;
-            node1.next = node2;
-            prev2.next = node1;
-        } else {
-            // General swap
-            ListNode temp = node1.next;
-            prev1.next = node2;
-            node1.next = node2.next;
-
-            prev2.next = node1;
-            node2.next = temp;
-        }
-
-        return dummy.next;
-    }
 
     private int getLength(ListNode head) {
         int len = 0;
