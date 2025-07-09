223c85c7ddf6b2549dacdb8d2812915f20f76ff6
SAMSHIBN A <151643144+SamShibinA@users.noreply.github.com>
2025-07-09 10:17:17 +0530

    Time: 147 ms (62.84%), Space: 61.3 MB (53.23%) - LeetHub

---
+++ 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        Stack<Integer> stack=new Stack<>();
+        stack.push(0);
+        int operations=0;
+        for(int n:nums){
+            while(!stack.isEmpty()&&stack.peek()>n){
+                stack.pop();
+            }
+
+            if(stack.peek()<n){
+            operations++;
+            stack.push(n);
+            }
+        }
+
+        return operations;
+    }
+}
