72edfc71d8afa893e3eda953754d35a0ab7a9919
SAMSHIBN A <151643144+SamShibinA@users.noreply.github.com>
2026-02-03 14:50:11 +0530

    Time: 20 ms (66.58%), Space: 46.2 MB (17.8%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,30 +1,39 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-           Arrays.sort(nums);
-           List<List<Integer>> res=new ArrayList<>();
+        Arrays.sort(nums);
 
-           for(int a=0;a<nums.length-3;a++){
-            if(a>0&&nums[a]==nums[a-1])continue;
-            for(int b=a+1;b<nums.length-2;b++){
-                if(b>a+1&&nums[b]==nums[b-1]) continue;
+        List<List<Integer>> answer=new ArrayList<>();
 
-                int c=b+1,d=nums.length-1;
+        int n=nums.length;
+
+        for(int a=0;a<n-3;a++){
+            if(a>0 && nums[a]==nums[a-1])continue;
+
+            for(int b=a+1;b<n-2;b++){
+                if(b>a+1 && nums[b]==nums[b-1])continue;
+
+                int c=b+1;
+                int d=n-1;
 
                 while(c<d){
-                    long sum=(long)nums[a]+nums[b]+nums[c]+nums[d];
-                    if(sum==target){
-                        res.add(Arrays.asList(nums[a],nums[b],nums[c],nums[d]));
-                        while(c<d&&nums[c]==nums[c+1])c++;
-                        while(c<d&&nums[d]==nums[d-1])d--;
 
+                    long sum=(long)nums[a] + nums[b] +nums[c] +nums[d];
+                    if(sum==target){
+                        answer.add(Arrays.asList(nums[a],nums[b],nums[c],nums[d]));
+                        while(c<d && nums[c]==nums[c+1])c++;
+                        while(c<d && nums[d]==nums[d-1])d--;
                         c++;
                         d--;
                     }
-                    else if(sum<target) c++;
-                    else d--;
+                    else if(sum>target)d--;
+                    else{
+                        c++;
+                    }
+
                 }
             }
-           }
-           return res;
+        }
+
+        return answer;
     }
 }
