f4e480802a5b109f5e07a1f4e5144c2e78b088ae
SAMSHIBN A <151643144+SamShibinA@users.noreply.github.com>
2026-02-11 11:58:02 +0530

    Time: 525 ms (27.77%), Space: 76.6 MB (68.59%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,85 @@
+class Solution {
+
+    class Edge{
+        int u,v,w;
+
+        Edge(int u,int v,int w){
+            this.u=u;
+            this.v=v;
+            this.w=w;
+        }
+    }
+
+    
+
+    class DSU{
+        int[] parent;
+        int[] rank;
+
+        DSU(int n){
+            parent=new int[n];
+            rank=new int[n];
+            for(int i=0;i<n;i++){
+                parent[i]=i;
+                rank[i]=0;
+            }
+        }
+
+        public int find(int x){
+            if(parent[x]!=x){
+                parent[x]=find(parent[x]);
+            }
+            
+            return parent[x];
+        }
+
+        public void union(int u,int v){
+            
+            int u_root=parent[u];
+            int v_root=parent[v];
+            if(u_root==v_root)return;
+
+            if(rank[u_root]>rank[v_root]){
+                parent[v_root]=u_root;
+            }
+            else if(rank[u_root]<rank[v_root]){
+                parent[u_root]=v_root;
+            }
+            else{
+                parent[v_root]=u_root;
+                rank[u_root]++;
+            }
+        }
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+        PriorityQueue<Edge> pq=new PriorityQueue<>((a,b) -> a.w - b.w);
+        
+        int n=points.length;
+        DSU dsu=new DSU(n);
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int w=Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]);
+                pq.offer(new Edge(i,j,w));
+            }
+        }
+
+
+        int answer=0;
+
+        while(!pq.isEmpty()){
+            Edge node=pq.poll();
+            
+            if(dsu.find(node.u)==dsu.find(node.v)){
+                continue;
+            }
+
+            dsu.union(node.u,node.v);
+            answer+=node.w;
+        }
+
+
+     return answer;
+    }
+}
