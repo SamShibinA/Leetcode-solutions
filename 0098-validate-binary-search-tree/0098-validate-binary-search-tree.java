8e690fafd31e31eb131ed3985f22f1826f189d83
SAMSHIBN A <151643144+SamShibinA@users.noreply.github.com>
2026-02-12 11:55:50 +0530

    Time: 0 ms (100%), Space: 45.2 MB (49.29%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -19,12 +19,12 @@ class Solution {
     public boolean isValidBST(TreeNode root) {
         if(root==null)return true;
 
-        if(!isValidBST(root.left))return false;
+        if(!isValidBST(root.left)) return false;
+
+        if(pre!=null && pre >= root.val) return false;
         
-        if(pre!=null && root.val<=pre)return false;
         pre=root.val;
 
-        
         return isValidBST(root.right);
     }
 }
