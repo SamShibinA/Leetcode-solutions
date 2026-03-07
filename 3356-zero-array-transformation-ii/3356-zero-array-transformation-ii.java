4a7e4fcc1b32c7f725819b01c64f097339572edf
SAMSHIBN A <151643144+SamShibinA@users.noreply.github.com>
2026-03-07 14:52:43 +0530

    Time: 1232 ms (6.74%), Space: 273.7 MB (23.97%) - LeetHub

---
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int zc=0;
+
+        for(int n:nums)if(n==0)zc++;
+        if(zc==nums.length) return 0;
+        
+        for(int i=0;i<queries.length;i++){
+            int start=queries[i][0];
+            int end=queries[i][1];
+            int val=queries[i][2];
+
+            for(int j=start;j<=end;j++){
+                if(nums[j]>0){
+                    nums[j]-=val;
+
+                    if(nums[j]<=0){
+                        zc++;
+                    }
+
+                    if(zc==nums.length) return i+1;
+                }
+            }
+        }
+        return -1;
+        
+    }
+}
