837284074ce433c8390d8ade1a977c0d0bf8ff59
SAMSHIBN A <151643144+SamShibinA@users.noreply.github.com>
2026-03-11 15:50:16 +0530

    Time: 2 ms (80.4%), Space: 47.9 MB (53.87%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,38 +1,41 @@
 class Solution {
+    int row,col;
     public void solve(char[][] board) {
-        int height=board.length,width=board[0].length;
+        this.row=board.length;
+        this.col=board[0].length;
 
-        for(int i=0;i<height;i++){
-            if(board[i][0]=='O')dfs(board,i,0);
-            if(board[i][width-1]=='O')dfs(board,i,width-1);
-        }
-        
-        for(int j=0;j<width;j++){
-            if(board[0][j]=='O')dfs(board,0,j);
-            if(board[height-1][j]=='O')dfs(board,height-1,j);
+        for(int i=0;i<row;i++){
+            if(board[i][0]=='O') dfs(i,0,board);
+            if(board[i][col-1]=='O')dfs(i,col-1,board);
         }
 
-        for(int i=0;i<height;i++){
-            for(int j=0;j<width;j++){
+
+        for(int j=0;j<col;j++){
+            if(board[0][j]=='O')dfs(0,j,board);
+            if(board[row-1][j]=='O')dfs(row-1,j,board);
+        }
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(board[i][j]=='O')board[i][j]='X';
                 if(board[i][j]=='S')board[i][j]='O';
             }
         }
 
-        
     }
-    
-    public void dfs(char[][] board,int i,int j){
-        if(i<0||j<0||i>=board.length||j>=board[0].length||board[i][j]=='S'||board[i][j]=='X')return;
+
+
+
+    public void dfs(int i,int j,char[][] board){
+        if(i<0 || i>=row ||j<0||j>=col || board[i][j]=='X' || board[i][j]=='S'){
+            return;
+        }
 
         board[i][j]='S';
 
-        dfs(board,i-1,j);
-        dfs(board,i,j+1);
-        dfs(board,i+1,j);
-        dfs(board,i,j-1);
-        
+        dfs(i-1,j,board);
+        dfs(i,j+1,board);
+        dfs(i+1,j,board);
+        dfs(i,j-1,board);   
     }
-    
-
-    }
+}
