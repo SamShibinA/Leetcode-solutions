d778e1aa25cc685d264efe20aa1a8a5a54aa30c5
SAMSHIBN A <151643144+SamShibinA@users.noreply.github.com>
2026-03-11 16:10:32 +0530

    Time: 4 ms (98.45%), Space: 47.6 MB (40.18%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,9 +1,6 @@
 class Solution {
     int m,n;
-
     int[][] dir={{-1,0},{0,1},{1,0},{0,-1}};
-
-
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         this.m=heights.length;
@@ -13,45 +10,43 @@ class Solution {
         boolean[][] atlantic=new boolean[m][n];
 
         for(int i=0;i<m;i++){
-            dfs(i,0,pacific,heights);
-            dfs(i,n-1,atlantic,heights);
+            dfs(pacific,heights,i,0);
+            dfs(atlantic,heights,i,n-1);
         }
 
-        for(int j=0;j<n;j++){
-            dfs(0,j,pacific,heights);
-            dfs(m-1,j,atlantic,heights);
+        for(int i=0;i<n;i++){
+            dfs(pacific,heights,0,i);
+            dfs(atlantic,heights,m-1,i);
         }
 
-        List<List<Integer>> answer =new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(pacific[i][j] && atlantic[i][j]){
-                    answer.add(Arrays.asList(i,j));
+                if(pacific[i][j]&&atlantic[i][j]){
+                    ans.add(Arrays.asList(i,j));
                 }
             }
         }
-        
-        return answer;
+
+        return ans;
+
     }
 
-
-   public void dfs(int r,int c,boolean[][] ocean,int[][] heights){
-        
+    public void dfs(boolean[][] ocean,int[][] heights,int r,int c){
+        if(ocean[r][c])return;
         ocean[r][c]=true;
 
-        for(int d[]:dir){
+        for(int[] d:dir){
             int nr=r+d[0];
             int nc=c+d[1];
 
-            if(nr<0 || nr>=m || nc<0 || nc>=n) continue;
+            if(nr<0||nr>=m||nc<0||nc>=n) continue;
 
-            if(!ocean[nr][nc] && heights[nr][nc]>=heights[r][c])
-            dfs(nr,nc,ocean,heights);
-
+            if(heights[r][c]<=heights[nr][nc]){
+                dfs(ocean,heights,nr,nc);
+            }
         }
 
-       
-   }
-     
+    }
 }
